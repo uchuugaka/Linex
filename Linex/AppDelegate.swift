911218fa12118fa69edfe75d9715ade65ec4aa12
Kaunteya Suryawanshi <kaunteya@kaunteys-macbook-pro.local>
2017-08-29 13:29:10 +0530

    Initial Commit

---
+++ Linex/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Linex
+//
+//  Created by Kaunteya Suryawanshi on 29/08/17.
+//  Copyright © 2017 Kaunteya Suryawanshi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Linex/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Linex
+//
+//  Created by Kaunteya Suryawanshi on 29/08/17.
+//  Copyright © 2017 Kaunteya Suryawanshi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
