04a6a1d4a13e557d0aa1ea350c488daa2c6f4691
Kaunteya Suryawanshi <kaunteya@kaunteys-macbook-pro.local>
2017-08-29 15:26:17 +0530

    Added Switch case

--- Line/SourceEditorCommand.swift
+++ Line/SourceEditorCommand.swift
@@ -9,15 +9,37 @@
 import Foundation
 import XcodeKit
 
+enum Options: String {
+    case OpenNewLine, NewCommentedLine, Duplicate
+    init(command: String) {
+        // Eg: com.kaunteya.Line.Duplicate
+        let bundle = Bundle.main.bundleIdentifier! + "."
+        let str = command.substring(from: bundle.endIndex)
+        self.init(rawValue: str)!
+    }
+}
+
 class SourceEditorCommand: NSObject, XCSourceEditorCommand {
     
     public func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Swift.Void) {
-        Swift.print("Here")
-        let buffer = invocation.buffer
-        let range1 = invocation.buffer.selections.firstObject as! XCSourceTextRange
-        let currentCursorlineOffset = range1.start.line
-        let currentLine = buffer.lines[currentCursorlineOffset] as! String
-        invocation.buffer.lines.insert(currentLine, at: currentCursorlineOffset)
+        switch Options(command: invocation.commandIdentifier) {
+        case .Duplicate:
+            let buffer = invocation.buffer
+            let range1 = invocation.buffer.selections.lastObject as! XCSourceTextRange
+            let currentCursorlineOffset = range1.start.line
+            let currentLine = buffer.lines[currentCursorlineOffset] as! String
+            invocation.buffer.lines.insert(currentLine, at: currentCursorlineOffset)
+
+        case .NewCommentedLine:
+            Swift.print("Here")
+            invocation.buffer.lines.insert("Commented line", at: 0)
+
+        case .OpenNewLine:
+            Swift.print("Here")
+            invocation.buffer.lines.insert("Open New line", at: 0)
+        }
+//        Swift.print("Here")
+
 
 
         completionHandler(nil)
